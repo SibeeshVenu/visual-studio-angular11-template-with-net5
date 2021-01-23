10a27b8f3becaaf83b0a1c4f8f886d0c68c46034
Sibeesh Venu <venu@medialesson.de>
2021-01-23 21:47:53 +0100

    configure UseProxyToSpaDevelopmentServer

--- VsNet5Ng11/VsNet5Ng11/Startup.cs
+++ VsNet5Ng11/VsNet5Ng11/Startup.cs
@@ -67,7 +67,7 @@ namespace VsNet5Ng11
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
